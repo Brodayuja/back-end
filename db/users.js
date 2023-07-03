4522abfadb4548cfb7ab98a321ce32c6f2d1925a
romanfaria <roman.faria@gmail.com>
2023-07-03 17:35:44 -0400

    modified /register and /login functionality. Login and Register now work through entire application

--- db/users.js
+++ db/users.js
@@ -1,20 +1,44 @@
-const client = require('./index');
-const bcrypt = require('bcrypt');
+const client = require("./index");
+const bcrypt = require("bcrypt");
 const SALT_COUNT = 10;
 
-async function createUser (name, username, password, email, avatar, location, website, favoriteBooks, aboutMe, isAdmin) {
-  const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
-  console.log("is hashPassword working?", hashedPassword)
+async function createUser(
+  name,
+  username,
+  password,
+  email,
+  avatar,
+  location,
+  website,
+  favoriteBooks,
+  aboutMe,
+  isAdmin
+) {
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+  console.log("is hashPassword working?", hashedPassword);
   try {
-    const {rows: [user]} = await client.query(
+    const {
+      rows: [user],
+    } = await client.query(
       `
     INSERT INTO users (name, username, password, email, avatar, location, website, "favoriteBooks", "aboutMe", "is_admin")
     VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
     RETURNING *;
     `,
-      [name, username, hashedPassword, email, avatar, location, website, favoriteBooks, aboutMe, isAdmin]
+      [
+        name,
+        username,
+        hashedPassword,
+        email,
+        avatar,
+        location,
+        website,
+        favoriteBooks,
+        aboutMe,
+        isAdmin,
+      ]
     );
-      console.log("is query working?", user)
+    console.log("is query working?", user);
     return user;
   } catch (error) {
     throw error;
@@ -23,27 +47,30 @@ async function createUser (name, username, password, email, avatar, location, we
 
 async function getAllUsers() {
   try {
-    const {rows} = await client.query(
+    const { rows } = await client.query(
       `
       SELECT *
       FROM users;
       `
-      );
-      
-      return rows;
-    } catch (error) {
-      throw error;
-    }
+    );
+
+    return rows;
+  } catch (error) {
+    throw error;
   }
-  
+}
+
 async function getUserByUsername(userName) {
   // first get the user
   try {
-    const {rows} = await client.query(`
+    const { rows } = await client.query(
+      `
       SELECT *
       FROM users
       WHERE username = $1;
-    `, [userName]);
+    `,
+      [userName]
+    );
     // if it doesn't exist, return null
     if (!rows || !rows.length) return null;
     // if it does:
@@ -52,21 +79,24 @@ async function getUserByUsername(userName) {
     // delete user.password;
     return user;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
-async function getUser({username, password}) {
+async function getUser(username, password) {
   if (!username || !password) {
     return;
   }
 
   try {
     const user = await getUserByUsername(username);
-    if(!user) return;
+
+    if (!user) return;
     const hashedPassword = user.password;
+    console.log(user.password);
     const passwordsMatch = await bcrypt.compare(password, hashedPassword);
-    if(!passwordsMatch) return;
+    console.log(passwordsMatch);
+    if (!passwordsMatch) return;
     delete user.password;
     return user;
   } catch (error) {
@@ -77,46 +107,54 @@ async function getUser({username, password}) {
 async function getUserById(userId) {
   // first get the user
   try {
-    const {rows: [user]} = await client.query(`
+    const {
+      rows: [user],
+    } = await client.query(
+      `
       SELECT *
       FROM users
       WHERE id = $1;
-    `, [userId]);
+    `,
+      [userId]
+    );
     // if it doesn't exist, return null
     if (!user) return null;
     // if it does:
     // delete the 'password' key from the returned object
-    delete user.password; 
-    return user;  
+    delete user.password;
+    return user;
   } catch (error) {
     throw error;
   }
 }
 
 async function updateUser(id, fields = {}) {
-  // build the set string 
-  const fieldsKeys = Object.keys(fields)
-  console.log(fieldsKeys, "fieldsKeys in update user")
-  
-  const mapOfStrings = fieldsKeys.map(
-    (key, index) => `"${ key }"=$${ index + 1 }`
-  )
-  console.log (mapOfStrings, "map of string in update user")
-  
-  const setString =  mapOfStrings.join(', ');
-  console.log (setString, "set string on update user")
+  // build the set string
+  const fieldsKeys = Object.keys(fields);
+  console.log(fieldsKeys, "fieldsKeys in update user");
+
+  const mapOfStrings = fieldsKeys.map((key, index) => `"${key}"=$${index + 1}`);
+  console.log(mapOfStrings, "map of string in update user");
+
+  const setString = mapOfStrings.join(", ");
+  console.log(setString, "set string on update user");
   // return early if this is called without fields
   if (setString.length === 0) {
     return;
   }
 
   try {
-    const { rows: [ user ] } = await client.query(`
+    const {
+      rows: [user],
+    } = await client.query(
+      `
       UPDATE users
-      SET ${ setString }
-      WHERE id=${ id }
+      SET ${setString}
+      WHERE id=${id}
       RETURNING *;
-    `, Object.values(fields));
+    `,
+      Object.values(fields)
+    );
 
     return user;
   } catch (error) {
@@ -124,14 +162,18 @@ async function updateUser(id, fields = {}) {
   }
 }
 
-
 async function destroyUser(id) {
   try {
-    const {rows: [user]} = await client.query(`
+    const {
+      rows: [user],
+    } = await client.query(
+      `
         DELETE FROM users
         WHERE id = $1
         RETURNING *;
-    `, [id]);
+    `,
+      [id]
+    );
     return user;
   } catch (error) {
     throw error;
@@ -145,5 +187,5 @@ module.exports = {
   getUserById,
   getUserByUsername,
   updateUser,
-  destroyUser
-}
+  destroyUser,
+};
